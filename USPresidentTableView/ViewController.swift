c33000873581f4735dfd48a0624e0e204ef488f5
fancytseng <55032412+fancytseng@users.noreply.github.com>
2021-02-09 19:43:05 -0800

    Assignment 3

--- USPresidentTableView/ViewController.swift
+++ USPresidentTableView/ViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    let names = ["George Washington", "John Adams", "Thomas Jerrferson", "James Madison", "James Monroe", "John Quincy Adams", "Andrew Jackson", "George H. W. Bush", "Ronald Reagan", "Bill Clinton", "George W. Bush", "Barack Obama", "Donald Trump", "Joe Biden"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tblView.delegate = self
+        tblView.dataSource = self
     }
+    
 
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
 }
 
